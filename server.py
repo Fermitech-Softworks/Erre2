d69587c51c222f7525e8c237f49123d96f96975d
Nemesis <lorenzo.balugani@gmail.com>
2019-09-29 17:25:32 +0200

    Backend is done, Frontend is almost ready

--- server.py
+++ server.py
@@ -2,12 +2,16 @@ from flask import Flask, session, url_for, redirect, request, render_template, a
 from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 import os
-from datetime import datetime
+import datetime
 import functools
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "Prova"
+app.config['UPLOAD_FOLDER'] = "./static"
+ALLOWED_EXTENSIONS = set(['txt', 'md', 'pdf', 'doc', 'docx'])
 db = SQLAlchemy(app)
 
 
@@ -20,13 +24,13 @@ class Author(db.Model):
     cognome = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.LargeBinary, nullable=False)
-    riassunti = db.relationship("Summary", backref="author")
+    riassunti = db.relationship("Summary", back_populates="autore")
 
     def __init__(self, nome, cognome, email, password):
         self.nome = nome
         self.cognome = cognome
         self.email = email
-        p = bytes(request.form["password"], encoding="utf-8")
+        p = bytes(password, encoding="utf-8")
         self.password = bcrypt.hashpw(p, bcrypt.gensalt())
 
     def __repr__(self):
@@ -41,7 +45,7 @@ class Course(db.Model):
     ramo = db.Column(db.String, nullable=False)
     anno = db.Column(db.Integer, nullable=False)
     semestre = db.Column(db.Integer, nullable=False)
-    riassunti = db.relationship("Summary", backref="course")
+    documenti = db.relationship("Summary", back_populates="corso")
 
     def __init__(self, nome, docente, ramo, anno, semestre):
         self.nome = nome
@@ -59,17 +63,21 @@ class Summary(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=False)
     descrizione = db.Column(db.String, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('author.aid'))
-    author = db.relationship("Author", back_populates="riassunti")
-    course_id = db.Column(db.Integer, db.ForeignKey('corso.cid'))
-    course = db.relationship("Course", back_populates="riassunti")
-    commit = db.relationship("Commit", backref="summary")
+    autore_id = db.Column(db.Integer, db.ForeignKey('author.aid'))
+    autore = db.relationship("Author", back_populates="riassunti")
+    corso_id = db.Column(db.Integer, db.ForeignKey('course.cid'))
+    corso = db.relationship("Course", back_populates="documenti")
+    commit = db.relationship("Commit", back_populates="summary")
+    downloads = db.Column(db.Integer)
+    filename = db.Column(db.String, nullable=False)
 
-    def __init__(self, nome, descrizione, author_id, course_id):
+    def __init__(self, nome, descrizione, author_id, course_id, filename):
         self.nome = nome
         self.descrizione = descrizione
-        self.author_id = author_id
-        self.course_id = course_id
+        self.autore_id = author_id
+        self.corso_id = course_id
+        self.downloads = 0
+        self.filename = filename
 
     def __repr__(self):
         return "Riassunto {}".format(self.nome)
@@ -84,17 +92,20 @@ class Commit(db.Model):
     summary = db.relationship("Summary", back_populates="commit")
 
     def __init__(self, descrizione, summary_id):
-        self.data = datetime.now()
+        self.data = datetime.datetime.now()
         self.summary_id = summary_id
         self.descrizione = descrizione
 
 
+nuovoutente = Author("", "", "", "")
+
+
 # UTILITIES
 
-def login(username, password):
-    user = Author.query.filter_by(email=username).first()
+def login(email, password):
+    user = Author.query.filter_by(email=email).first()
     try:
-        return bcrypt.checkpw(bytes(password, encoding="utf-8"), user.passwd)
+        return bcrypt.checkpw(bytes(password, encoding="utf-8"), user.password)
     except AttributeError:
         # Se non esiste l'Utente
         return False
@@ -102,6 +113,11 @@ def login(username, password):
 
 def find_user(username):
     return Author.query.filter_by(email=username).first()
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # Decorators
@@ -122,7 +138,7 @@ def login_or_redirect(f):
     @functools.wraps(f)
     def func(*args, **kwargs):
         if not session.get("username"):
-            return redirect(url_for('page_login'))
+            return redirect(url_for('page_dashboard'))
         return f(*args, **kwargs)
 
     return func
@@ -140,20 +156,20 @@ def page_home():
 
 @app.route('/dashboard')
 def page_dashboard():
-    latest_changes = Commit.query.order_by(Commit.data.desc()).Join(Summary).limit(10)
+    latest_changes = Commit.query.join(Summary).order_by(Commit.cid.desc()).limit(10)
     materie = Course.query.all()
-    return render_template("dashboard.htm", latest_changes=latest_changes, materie=materie)
+    return render_template("dashboard.htm", changes=latest_changes, materie=materie)
 
 
 @app.route('/dashboard/course/<int:cid>')
 def page_filter_course(cid):
-    riassunti = Summary.query.filter_by(course_id=cid).join(Author).all()
+    riassunti = Summary.query.filter_by(corso_id=cid).join(Author).all()
     return render_template("/riassunti_list.htm", riassunti=riassunti)
 
 
 @app.route('/dashboard/author/<int:aid>')
 def page_filter_author(aid):
-    riassunti = Summary.query.filter_by(author_id=aid).join(Author).all()
+    riassunti = Summary.query.filter_by(autore_id=aid).join(Author).all()
     return render_template("/riassunti_list.htm", riassunti=riassunti)
 
 
@@ -163,10 +179,13 @@ def page_riassunti_list():
     return render_template("/riassunti_list.htm", riassunti=riassunti)
 
 
-@app.route('/dashboard/summary/<int:sid>')
-def page_riassunto(sid):
-    riassunto = Summary.query.get_or_404(sid)
-    changelog = Commit.query.filter_by(summary_id=sid).order_by(Commit.data.desc()).all()
+@app.route("/dashboard/summaries/summary/<int:sid>")
+def page_inspect_riassunto(sid):
+    riassunto = Summary.query.filter_by(sid=sid).first()
+    riassunto.downloads += 1
+    db.session.commit()
+    return redirect("/static/" + riassunto.filename.replace(" ", "_"))
+
 
 # Pages and functions for the administrator
 
@@ -188,14 +207,133 @@ def func_login():
 @login_or_403
 def page_administration():
     materie = Course.query.all()
-    riassunti = Summary.query.all()
-    return render_template("/administration/dashboard.htm", materie=materie, riassunti=riassunti)
+    riassunti = Summary.query.join(Author).all()
+    utente = find_user(session['username'])
+    return render_template("/administration/dashboard.htm", materie=materie, riassunti=riassunti, utente=utente)
 
 
-# TODO: add a way to upload and update summaries and to add courses
+@app.route("/add_materia", methods=['GET', 'POST'])
+@login_or_403
+def page_add_materia():
+    utente = find_user(session['username'])
+    if request.method == 'GET':
+        nuovocorso = Course("", "", "", 0, 0)
+        return render_template("/administration/materia.htm", utente=utente, corso=nuovocorso, mode=0)
+
+    nuovamateria = Course(request.form.get("nome"), request.form.get("docente"), request.form.get("ramo"),
+                          request.form.get("anno"), request.form.get("semestre"))
+    db.session.add(nuovamateria)
+    db.session.commit()
+    return redirect(url_for('page_administration'))
+
+
+@app.route("/edit_materia/<int:cid>", methods=['GET', 'POST'])
+@login_or_403
+def page_edit_materia(cid):
+    utente = find_user(session['username'])
+    corso = Course.query.get_or_404(cid)
+    if request.method == 'GET':
+        return render_template("/administration/materia.htm", utente=utente, corso=corso, mode=1)
+    corso.nome = request.form.get("nome")
+    corso.docente = request.form.get("docente")
+    corso.ramo = request.form.get("ramo")
+    corso.anno = request.form.get("anno")
+    corso.semestre = request.form.get("semestre")
+    db.session.commit()
+    return redirect(url_for('page_administration'))
+
+
+@app.route("/add_riassunto", methods=['GET', 'POST'])
+@login_or_403
+def page_add_riassunto():
+    utente = find_user(session['username'])
+    if request.method == 'GET':
+        corsi = Course.query.all()
+        return render_template("/administration/riassunto.htm", utente=utente, corsi=corsi)
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    nuovoriassunto = Summary(request.form.get("nome"), request.form.get("descrizione"), int(utente.aid),
+                             int(request.form["listamaterie"]), file.filename)
+    db.session.add(nuovoriassunto)
+    riassunto = Summary.query.filter_by(filename=file.filename).first()
+    nuovocommit = Commit("Riassunto aggiunto a Erre2.", int(riassunto.sid))
+    db.session.add(nuovocommit)
+    db.session.commit()
+    return redirect(url_for('page_administration'))
+
+
+@app.route("/update_riassunto/<int:sid>", methods=['GET', 'POST'])
+@login_or_403
+def page_update_riassunto(sid):
+    utente = find_user('username')
+    riassunto = Summary.query.get_or_404(sid)
+    if request.method == 'GET':
+        return render_template("/administration/update.htm", riassunto=riassunto, utente=utente)
+    try:
+        os.remove("./static/" + riassunto.filename)
+    except FileNotFoundError:
+        pass
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    riassunto.filename = file.filename
+    nuovocommit = Commit(request.form.get('descrizione'), riassunto.sid)
+    db.session.add(nuovocommit)
+    db.session.commit()
+    return redirect(url_for('page_administration'))
+
+
+@app.route("/delete_riassunto/<int:sid>")
+@login_or_403
+def func_delete_riassunto(sid):
+    riassunto = Summary.query.get_or_404(sid)
+    commits = Commit.query.filter_by(summary_id=sid)
+    for committ in commits:
+        db.session.delete(committ)
+    try:
+        os.remove("./static/" + riassunto.filename)
+    except FileNotFoundError:
+        pass
+    db.session.delete(riassunto)
+    db.session.commit()
+    return redirect(url_for('page_administration'))
+
+
+@app.route("/delete_materia/<int:cid>")
+@login_or_403
+def func_delete_materia(cid):
+    materia = Course.query.get_or_404(cid)
+    riassunti = Summary.query.filter_by(corso_id=cid)
+    for riassunto in riassunti:
+        commits = Commit.query.filter_by(summary_id=riassunto.sid)
+        for committ in commits:
+            db.session.delete(committ)
+        try:
+            os.remove("./files/" + riassunto.filename)
+        except FileNotFoundError:
+            pass
+        db.session.delete(riassunto)
+    db.session.delete(materia)
+    db.session.commit()
+    return redirect(url_for('page_administration'))
 
 
 if __name__ == "__main__":
     # Aggiungi sempre le tabelle non esistenti al database, senza cancellare quelle vecchie
     db.create_all()
-    app.run()
+    autore = Author.query.all()
+    if len(autore) == 0:
+        db.session.add(nuovoutente)
+        db.session.commit()
+    app.run(debug=True)
